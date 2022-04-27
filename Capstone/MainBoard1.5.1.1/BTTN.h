e9b8f38fd39b384ca168a3c1c7ab39b98f40c342
ArticunoSystems <93009785+ArticunoSystems@users.noreply.github.com>
2022-04-27 08:36:13 -0500

    Add files via upload

---
+++ Capstone/MainBoard1.5.1.1/BTTN.h
@@ -0,0 +1,179 @@
+int UPbuttonState;             // the current reading from the input pin
+int DOWNbuttonState;             // the current reading from the input pin
+int SELbuttonState;             // the current reading from the input pin
+int BACKbuttonState;             // the current reading from the input pin
+int lastUPButtonState = LOW;   // the previous reading from the input pin
+int lastDOWNButtonState = LOW;   // the previous reading from the input pin
+int lastSELButtonState = LOW;   // the previous reading from the input pin
+int lastBACKButtonState = LOW;   // the previous reading from the input pin
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+unsigned long CurBttn_Time;
+unsigned long LoopBttn_Time;
+
+int lvl1 = 0;
+int lvl2 = 0;
+int lvl3 = 0;
+int lvl4 = 1;
+int curlvl = 0; //0=default display, 
+
+int UP = 25; //pin for UP button
+int DOWN = 14; //pin for BACK button
+int SEL = 27; //pin for SELECT button
+int BACK = 26; //pin for BACK button
+
+void UPbttn(){
+  lcd.clear();
+  LoopBttn_Time = CurBttn_Time;
+  switch (curlvl){
+    case 0:
+      break;
+    case 1: //level 1
+      if(lvl1 > 0){
+        lcd.clear();
+        lvl1 -=1;
+      }
+      break;
+    case 2: //level 2
+      if(lvl2 > 0){
+        lcd.clear();
+        lvl2 -=1;
+      }
+      break;
+    case 3: //level 3
+      if(lvl3 > 1){
+        lcd.clear();
+        lvl3 -=1;
+      }
+      break;
+  }
+}
+void DOWNbttn(){
+  LoopBttn_Time = CurBttn_Time;
+  switch (curlvl){
+    case 0:
+      break;
+    case 1: //level 1
+      if(lvl1 < 4){
+        lcd.clear();
+        lvl1 +=1;
+      }
+      break;
+    case 2: //level 2
+      if((lvl2 < 4)&&(lvl1 == 3)){
+        lcd.clear();
+        lvl2 +=1;
+      }
+      else if(lvl2 < 2){
+        lcd.clear();
+        lvl2 +=1;
+      }
+      break;
+    case 3: //level 3
+      if(lvl3 < 5){
+        lcd.clear();
+        lvl3 +=1;
+      }
+      break;
+  }
+}
+void SELbttn(){
+  LoopBttn_Time = CurBttn_Time;
+  if(curlvl < 3){
+    lcd.clear();
+    curlvl += 1;
+  }
+}
+void BACKbttn(){
+  LoopBttn_Time = CurBttn_Time;
+  digitalWrite(BZZpin, LOW);
+  if(curlvl > 0){
+    lcd.clear();
+    lvl2 = 0;
+    curlvl -= 1;
+  }
+}
+
+void IdleBttn(){
+  CurBttn_Time = millis();
+  if(CurBttn_Time > LoopBttn_Time +15000){
+    Serial.println("Idle Menu");
+    curlvl = 0;
+    lvl1 = 0;
+    lvl2 = 0;
+    lvl3 = 0;
+
+    LoopBttn_Time = CurBttn_Time;
+    digitalWrite(BZZpin, LOW);
+  }
+}
+
+void BTTNSetup(){
+  pinMode(UP, INPUT);
+  pinMode(DOWN, INPUT);
+  pinMode(SEL, INPUT);
+  pinMode(BACK, INPUT);
+}
+
+void BTTNLoop(){
+  //----UP Button Debounce----
+  int UPreading = digitalRead(UP);
+  if (UPreading != lastUPButtonState) {
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (UPreading != UPbuttonState) {
+      UPbuttonState = UPreading;
+      if(UPbuttonState == HIGH){
+        UPbttn();
+      }
+    }
+  }
+  lastUPButtonState = UPreading;
+
+  //----DOWN Button Debounce----
+  int DOWNreading = digitalRead(DOWN);
+  if (DOWNreading != lastDOWNButtonState) {
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (DOWNreading != DOWNbuttonState) {
+      DOWNbuttonState = DOWNreading;
+      if(DOWNbuttonState == HIGH){
+        DOWNbttn();
+      }
+    }
+  }
+  lastDOWNButtonState = DOWNreading;
+
+  //----SELECT Button Debounce----
+  int SELreading = digitalRead(SEL);
+  if (SELreading != lastSELButtonState) {
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (SELreading != SELbuttonState) {
+      SELbuttonState = SELreading;
+      if(SELbuttonState == HIGH){
+        SELbttn();
+      }
+    }
+  }
+  lastSELButtonState = SELreading;
+
+  //----BACK Button Debounce----
+  int BACKreading = digitalRead(BACK);
+  if (BACKreading != lastBACKButtonState) {
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (BACKreading != BACKbuttonState) {
+      BACKbuttonState = BACKreading;
+      if(BACKbuttonState == HIGH){
+        BACKbttn();
+      }
+    }
+  }
+  lastBACKButtonState = BACKreading;
+}
---
+++ Capstone/MainBoard1.5.1.2/WATER.h
@@ -0,0 +1,114 @@
+unsigned long CurWat_Time, LoopWat_Time; //Used for timing conditions
+int WTmp = 0; //for debugging using serial monitor
+
+//int airPower = 12; //GPIO for Compressor Power Toggle, Relay1
+//int airMain = 15; //GPIO for Air Compressor Valve, Relay2
+//int waterMain = 4; //GPIO for Main Water Valve, Relay3
+//int z1Valve = 16; //GPIO for Zone 1 Valve, Relay4
+//int z2Valve = 17; //GPIO for Zone 2 Valve, Relay5
+//int z3Valve = 18; //GPIO for Zone 3 Valve, Relay6
+
+int waterInt = 1000*30; //Minimum amount of time to wait between waterings
+int waterLimit = 1; //rain in inches to prevent watering
+int lastWater = 0; //millis time of last water sequence
+bool waterState = false; //If the system is currently being watered
+
+int wMillisDif = 0;
+
+void WaterSetup(){
+  pinMode(waterMain,OUTPUT);
+  pinMode(z1Valve,OUTPUT);
+  pinMode(z2Valve,OUTPUT);
+  pinMode(z3Valve,OUTPUT);
+  pinMode(airMain,OUTPUT);
+  pinMode(airPower,OUTPUT);
+}
+
+void Sprinkle(){
+  if(waterState != true){
+    Serial.println("----WATERING SYSTEM----");
+    waterState = true; //signifies system is watering
+    purgeState = 0; //Update state of purge
+    LoopWat_Time = millis();
+  }
+  CurWat_Time = millis();
+  if((CurWat_Time >= LoopWat_Time) && (CurWat_Time <= (LoopWat_Time + 5000))){ //Water Zone 1
+    if(WTmp == 1){
+      Serial.println("Watering Zone 1");
+      WTmp++;
+    }
+    digitalWrite(waterMain,HIGH);//OPEN Water Main
+    digitalWrite(z1Valve,HIGH);
+    digitalWrite(z2Valve,LOW);
+    digitalWrite(z3Valve,LOW);
+    digitalWrite(airPower,LOW);//Turn OFF Air Compressor
+    z1Check();
+  }
+  else if((CurWat_Time > (LoopWat_Time+5000)) && (CurWat_Time <= (LoopWat_Time+10000))){ //Water Zone 2
+    if(WTmp == 2){
+      Serial.println("Watering Zone 2");
+      WTmp++;
+    }
+    digitalWrite(z1Valve,LOW);
+    digitalWrite(z2Valve,HIGH);
+    z2Check();
+  }
+  else if((CurWat_Time > (LoopWat_Time+10000)) && (CurWat_Time <= (LoopWat_Time+15000))){ //Water Zone 3
+    if(WTmp == 3){
+      Serial.println("Watering Zone 3");
+      WTmp++;
+    }
+    digitalWrite(z2Valve,LOW);
+    digitalWrite(z3Valve,HIGH);
+    z3Check();
+  }
+  else if((CurWat_Time > (LoopWat_Time+15000)) && (CurWat_Time <= (LoopWat_Time+20000))){ //End Watering
+    digitalWrite(z3Valve,LOW);
+    digitalWrite(waterMain,LOW);
+    Serial.println("----WATERING COMPLETED----");
+    WTmp = 0;
+    waterState = false; //signifies system is NOT watering
+    lastWater = millis();
+    if(CalFlow == true){
+      tarFlow = (Calz1Flow + Calz2Flow + Calz3Flow)/3;
+      Serial.println("Target Flow Rate Calibrated");
+      CalFlow = false;
+    }
+  }
+  else{
+    Serial.println("Water Timing Error");
+  }
+}
+
+void waterManual(){
+  if((waterState == false)&&(purgeState != 1)){
+    Sprinkle();
+  }
+}
+void WaterCheck(){
+  CurWat_Time = millis();
+  if((waterState == true)&&(purgeState != 1)){
+    Sprinkle();
+  }
+  if(CurWat_Time >= lastWater){
+    wMillisDif = CurWat_Time - lastWater;
+  }
+  if(CurWat_Time < lastWater){
+    wMillisDif = (4294967295 - lastWater) + CurWat_Time;
+  }
+  if((wMillisDif > waterInt)&&(purgeState !=1)){ //Time since last water comparison
+    if(waterState == false){
+      Serial.println("enough time has passed since last water");
+      if((fRain0 < waterLimit) || (fRain1 < waterLimit)){ //Rain Forecast check
+        if(WTmp == 0){
+          Serial.println("Forecast meets watering criteria for the day");
+          WTmp++;
+        }
+        Sprinkle();
+      }
+      if(fRain0 > waterLimit){
+        lastWater = millis();
+      }
+    }
+  }
+}
